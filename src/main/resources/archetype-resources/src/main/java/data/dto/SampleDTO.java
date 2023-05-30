e1298375cada0dd22ac3ed9d04eea363b0ab1fd7
joalvarez <alvarez.joaquinez@gmail.com>
2023-05-30 20:07:56 -0300

    fix: fixed bugs version

--- src/main/resources/archetype-resources/src/main/java/data/dto/SampleDTO.java
+++ src/main/resources/archetype-resources/src/main/java/data/dto/SampleDTO.java
@@ -1,7 +1,9 @@
 package ${package}.data.dto;
 
 import com.joalvarez.baseframework.data.dto.BaseDTO;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class SampleDTO implements BaseDTO {
 
 }
